a342117e3b0dc38c848ac4458c18abe0d55a3887
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-07-28 20:25:31 +0700

    Implicit Intent dial number

--- app/src/main/java/com/freshcipher/myintentapp/MainActivity.kt
+++ app/src/main/java/com/freshcipher/myintentapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.freshcipher.myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
     }
 
     override fun onClick(v: View?){
@@ -30,6 +34,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWitDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy")
                 moveWitDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
                 startActivity(moveWitDataIntent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "08123456789"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
             }
 
         }
